563146ec6cdb963e12c204242374d88208775844
Lou-HTX <luismoreno.it@gmail.com>
2017-08-20 15:03:26 -0500

    checkout payment working

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,14 @@ router.use(expressValidator());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    var successMsg = req.flash('success')[0];
     Product.find(function(err, docs) {
         var productChunks = [];
         var chunkSize = 3;
         for (var i = 0; i < docs.length; i += chunkSize) {
             productChunks.push(docs.slice(i, i + chunkSize));
         }
-        res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+        res.render('shop/index', { title: 'Shopping Cart', products: productChunks, successMsg: successMsg, noMessages: !successMsg});
     });
 });
 
@@ -63,5 +64,65 @@ router.get('/shopping-cart', function(req, res, next) {
 });
 
 
+router.get('/checkout', function(req, res, next) {
+    if (!req.session.cart) {
+        return res.redirect('/shopping-cart');
+    }
+    var cart = new Cart(req.session.cart);
+    var errMsg = req.flash('error')[0];
+    res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
+});
+
+
+// router.post('/checkout', function(req, res, next) {
+//     if (!req.session.cart) {
+//         return res.redirect('/shopping-cart');
+//     }
+//     var cart = new Cart(req.session.cart);
+
+//     var stripe = require("stripe")("sk_test_v86ihVaaNGft27qeGzF8r6JK");
+
+//     stripe.charges.create({
+//     amount: cart.totalPrice * 100,
+//     currency: "usd",
+//     source: req.body.stripeToken, // obtained with Stripe.js
+//     description: "Test charge"
+//     }, function(err, charge) {
+//     // asynchronously called
+//     if (err) {
+//         req.flash('error', err.message);
+//         return res.redirect('/checkout');
+//     }
+//     req.flash('success', 'Successfully placed order!');
+//     req.cart = null;
+//     res.redirect('/');
+//     }); 
+// });
+
+router.post('/charge', function(req, res, next) {
+    if (!req.session.cart) {
+        return res.redirect('/shopping-cart');
+    }
+    var cart = new Cart(req.session.cart);
+
+    var stripe = require("stripe")("sk_test_v86ihVaaNGft27qeGzF8r6JK");
+
+    var token = req.body.stripeToken;
+
+    var charge = stripe.charges.create({
+        amount: cart.totalPrice * 100,
+        currency: "usd",
+        description: "Test charge",
+        source: token,
+    }, function(err, charge) {
+        if (err) {
+        req.flash('error', err.message);
+        return res.redirect('/checkout');
+    }
+    req.flash('success', 'Successfully placed order!');
+    req.cart = null;
+    res.redirect('/');
+    });   
+});
 
 module.exports = router;
